c3f8267ff363498a008600f4c4cac56e73c3a3dd
knightfeng <edwardfeng@Edwards-MacBook-Air.local>
2017-06-14 21:09:40 -0700

    First commit, no working auto-add-char implementation

---
+++ background_script.js
@@ -0,0 +1,171 @@
+var dictionary = {};
+var currentKnownWords = 0;
+var currentUnknownWords = 0;
+var currentKnownChars = 0;
+var currentUnknownChars = 0;
+
+var currentStatePerTab = {};
+
+var enableAutoAddChar;
+
+/* Add when implemented button correctly.
+chrome.runtime.sendMessage({want: "enableAutoAddChar"}, function(response) {
+	enableAutoAddChar = response.enableAutoAddChar;
+	
+});
+
+alert(enableAutoAddChar);
+*/
+
+function parseDictionary(progressEvent){
+    // Entire file
+    // By lines
+    var lines = this.responseText.split('\n');
+    for(var i = 0; i < lines.length; i++){
+        if(lines[i][0] == "#") {
+            continue;
+        }
+
+        // var parsed = /([^\s]+) ([^\s]+) \[(.+)\] \/(.+)\//i.exec(lines[i]);
+        var parsed = /([^\s]+) ([^\s]+)/i.exec(lines[i]);
+        var traditional = parsed[1];
+        var simplified = parsed[2];
+        // var pinin = parsed[3].split(' ');
+        // var explanation = parsed[4].split('/');
+
+        if (/\w+/g.test(traditional)) {
+            continue;
+        }
+
+        var entry = {
+            'traditional': traditional,
+            'simplified': simplified,
+            // 'pinin': pinin,
+            // 'explanation': explanation,
+        }
+        dictionary[simplified] = entry;
+        dictionary[traditional] = entry;
+    }
+    // console.log(Object.keys(dictionary).length);
+};
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", parseDictionary);
+// https://www.mdbg.net/chindict/chindict.php?page=cedict
+oReq.open("GET", chrome.runtime.getURL("dictionaries/cedict_ts.u8"));
+oReq.send();
+
+var knownWords;
+loadKnownChars();
+
+function loadKnownChars() {
+    knownWords = new Set();
+
+    chrome.storage.sync.get("data", function(storage) {
+        if (chrome.runtime.error) {
+        	console.log("Runtime error.");
+        	return;
+        }
+        if (storage.data === null) { return; }
+
+        knownWords = new Set(storage.data);
+        // console.log('Loaded ' + knownWords.size + ' known words: ', knownWords);
+    });
+}
+
+function saveKnownChars() {
+    console.log('Saved ' + knownWords.size + ' known words: ', knownWords);
+
+    chrome.storage.sync.set({ data: Array.from(knownWords) }, function() {
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
+    });
+}
+
+function addAllToKnown(arr) {
+	for (var i=0; i<arr.length;i++) {
+		addToKnown(arr[i]);
+	}
+}
+
+function addToKnown(text) {
+    if(dictionary[text] !== undefined) {
+        
+		if(text.length > 1) {
+			knownWords.add(text);
+
+			for(var i = 0; i < text.length; i++){ 
+				if(dictionary[text[i]] !== undefined) {
+					knownWords.add(text[i]);
+				}
+			}
+		}
+
+		knownWords.add(text);
+		
+    }
+    
+    // for (var i=0; i<text.length; i++) {
+    // 	if(dictionary[text[i]] !== undefined) {
+	   //      knownWords.add(text[i]);
+	   //  }
+    // }
+}
+
+function removeFromKnown(text) {
+    knownWords.delete(text);
+}
+
+chrome.runtime.onConnect.addListener(function(port) {
+	if (port.name == "knownchinese-dict") {
+		port.onMessage.addListener(function(msg) {
+			port.postMessage({
+				id: msg.id,
+				char: msg.char,
+				knownword: knownWords.has(msg.text),
+			});
+		});
+	} else if (port.name == "knownchinese-saver") {
+		port.onMessage.addListener(function(msg) {
+			if (msg.save) {
+				addToKnown(msg.word);
+			} else {
+				removeFromKnown(msg.word);
+			}
+			saveKnownChars();
+		});
+	}
+});
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+	if (request.want == "dictionary") {
+		sendResponse({dictionary: dictionary});
+	} else if (request.want == "knownwords") {
+		sendResponse({knownwords: Array.from(knownWords)});
+	} else if (request.want == "options") {
+		chrome.tabs.create({'url': chrome.extension.getURL('options.html'), 'selected': true});
+	} else if (request.want == "saveall") {
+		if(request.overwrite) {
+			knownWords = new Set();
+		}
+		addAllToKnown(request.input);
+		saveKnownChars();
+		sendResponse({knownwords: Array.from(knownWords)});
+	}
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		
+		if (currentStatePerTab[tabs[0].id] === undefined) {
+			currentStatePerTab[tabs[0].id] = 0;
+			chrome.tabs.sendMessage(tabs[0].id, {enable: true});
+		} else {
+			delete currentStatePerTab[tabs[0].id];
+			chrome.tabs.sendMessage(tabs[0].id, {enable: false});
+		}
+	});
+});
+
---
+++ content_script.js
@@ -0,0 +1,195 @@
+var dictionary = null;
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (request.enable) {
+			refresh();
+		} else {
+			removeHighlights();
+		}
+});
+
+var dict = chrome.runtime.connect({name: "knownchinese-dict"});
+var saver = chrome.runtime.connect({name: "knownchinese-saver"});
+dict.onMessage.addListener(function(msg) {
+
+	el = document.getElementById(msg.id);
+
+	var cssclass;
+	if (msg.char) {
+		if (msg.knownword) {
+			cssclass = 'knownchar';
+		} else {
+			cssclass = 'unknownchar';
+		}
+	} else {
+		if (msg.knownword) {
+			cssclass = 'knownword';
+		} else {
+			cssclass = 'unknownword';
+		}
+	}
+
+	el.classList.add(cssclass);
+});
+
+(function() {
+    var node = document.createElement('style');
+    document.body.appendChild(node);
+    window.addStyleString = function(str) {
+        node.innerHTML = str;
+    }
+}());
+
+addStyleString('\
+                          .knownchinese-element { font-style: inherit; }\
+    .knownchinese-enabled .knownchinese-element.unknownword { background: -webkit-linear-gradient(left, transparent, rgba(255,0,0,.2), transparent); }\
+    .knownchinese-enabled .knownchinese-element.unknownword .knownchinese-element.knownchar { background: -webkit-linear-gradient(left, transparent, rgba(0,0,255,.2), transparent); }\
+    .knownchinese-enabled .knownchinese-element.knownword { background: -webkit-linear-gradient(left, transparent, rgba(0,255,0,.33), transparent); }\
+');
+
+var currentState = 0;
+
+window.addEventListener('load', autoHighlight);
+document.addEventListener('keydown', keyboardShortcuts);
+document.body.classList.add('knownchinese', 'knownchinese-enabled');
+
+function refresh() {
+	if (dictionary === null) {
+		chrome.runtime.sendMessage({want: "dictionary"}, function(response) {
+			dictionary = response.dictionary;
+			refresh();
+		});
+		return;
+	}
+
+    walk(document.body);
+}
+
+function autoHighlight(event) {
+    console.log("All resources finished loading!");
+    //Check to see if page has any chinese.
+    refresh();
+    event.stopPropagation();
+}
+
+
+function keyboardShortcuts(ev) {
+    if (ev.shiftKey && ev.altKey && ev.keyCode==82) { // r
+        removeHighlights();
+        refresh();
+        ev.stopPropagation();
+    } else if (ev.shiftKey && ev.altKey && ev.keyCode==65) { // a
+    	saver.postMessage({save:true, word:getSelectionText()});
+    	removeHighlights();
+        refresh();
+        ev.stopPropagation();
+    } else if (ev.shiftKey && ev.altKey && ev.keyCode==68) { // d
+        saver.postMessage({save:false, word:getSelectionText()});
+    	removeHighlights();
+        refresh();
+        ev.stopPropagation();
+    } else if (ev.shiftKey && ev.altKey && ev.keyCode==84) { // t
+        removeHighlights();
+        ev.stopPropagation();
+    } else if (ev.shiftKey && ev.altKey && ev.keyCode==79) { // o
+        chrome.runtime.sendMessage({want: "options"});
+        ev.stopPropagation();
+    }
+}
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+// Our walker function
+function walk(node) {
+    // http://is.gd/mwZp7E
+    var child, next;
+    switch (node.nodeType) {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+        case 3: // Text node
+            highlightWords(node);
+            break;
+    }
+}
+
+function randId() {
+	return "knownchinese-"+Math.random().toString(36).substr(2, 10);
+}
+
+function highlightWords(textNode) {
+    var content = textNode.data;
+
+	var temp = document.createElement('div');
+
+    var newContent = "";
+    var word = "";
+    var anyChange = false;
+    var insideTag = false;
+    for(var i=0; i<content.length; i++) {
+        if (content[i] == "<") {
+            insideTag  = true;
+        } else if (content[i] == ">") {
+            insideTag = false;
+        }
+
+        if (!insideTag && dictionary[content[i]] !== undefined) {
+            anyChange = true;
+
+            word = content[i];
+            while (i+1 < content.length && dictionary[word+content[i+1]] !== undefined) {
+                word += content[++i];
+            }
+            
+            var id = randId();
+            dict.postMessage({text: word, id: id, char:false});
+            newContent += "<i class='knownchinese-element' id='"+id+"'>";
+			for (var j=0; j<word.length;j++) {
+				var id = randId();
+            	dict.postMessage({text: word[j], id: id, char:true});
+                newContent += "<i class='knownchinese-element' id='"+id+"'>"+word[j]+"</i>";
+            }
+            newContent += "</i>";
+        } else {
+            newContent += content[i];
+        }
+    }
+
+    if (anyChange) {
+    	temp.innerHTML = newContent;
+
+	    while (temp.firstChild) {
+	        textNode.parentNode.insertBefore(temp.firstChild, textNode);
+	    }
+	    textNode.parentNode.removeChild(textNode);
+    }
+}
+
+function removeHighlights() {
+    
+    var nodes = null;
+	while((nodes = document.getElementsByClassName("knownchinese-element")).length > 0) {
+
+		for (var i=0; i<nodes.length; i++) {
+			var node = nodes[i];
+			node.parentNode.insertBefore(document.createTextNode(node.textContent), node);
+	    	node.parentNode.removeChild(node);
+		}
+	}
+}
